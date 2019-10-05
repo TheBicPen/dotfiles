ef42213c397d595813cef2ae3e95ec5f1384e9c1
Alex <mashkal2000@gmail.com>
2019-10-05 15:47:14 -0400

    update osu launch script to allow parsing multiple files

--- scripts/osu-launch.sh
+++ scripts/osu-launch.sh
@@ -1,8 +1,8 @@
 file=
 if [ $# -gt 0 ]; then
-	file="$1"
-	echo "$file"
+	file="$file $@"
 fi
+echo "launching osu! with files $file"
 
-LD_LIBRARY_PATH="/home/alex/git-repos/osu/osu.Desktop/bin/Release/netcoreapp2.2" \
-/home/alex/git-repos/osu/osu.Desktop/bin/Release/netcoreapp2.2/linux-x64/osu\! "$file"
+LD_LIBRARY_PATH="/home/alex/git-repos/osu/osu.Desktop/bin/Release/netcoreapp2.2/linux-x64" \
+/home/alex/git-repos/osu/osu.Desktop/bin/Release/netcoreapp2.2/linux-x64/osu\! $file
